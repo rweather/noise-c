a6f710c7a8be6a55dccb75109148737a3639765f
Rhys Weatherley <rhys.weatherley@gmail.com>
2016-04-19 19:03:25 +1000

    Update docs

--- search/variables_3.js
+++ search/variables_3.js
@@ -1,7 +1,9 @@
 var searchData=
 [
   ['decrypt',['decrypt',['../structNoiseCipherState__s.html#a465d41a66eb999d594e97f11cbbbcb00',1,'NoiseCipherState_s']]],
+  ['derive_5fpublic_5fkey',['derive_public_key',['../structNoiseDHState__s.html#aa3abd0225f5cc1efa5fae5903a7b8948',1,'NoiseDHState_s']]],
   ['destroy',['destroy',['../structNoiseCipherState__s.html#abd576b311efc077bcb296a3a7f82c45a',1,'NoiseCipherState_s::destroy()'],['../structNoiseHashState__s.html#add43fdc210ad2900cf23075081f1a601',1,'NoiseHashState_s::destroy()'],['../structNoiseDHState__s.html#a19deb64f0c9b893f8f736db347823c81',1,'NoiseDHState_s::destroy()']]],
+  ['dh_5ffixed_5fephemeral',['dh_fixed_ephemeral',['../internal_8h.html#aff8cee2a4e22872a86b625248098dc68',1,'NoiseHandshakeState_s']]],
   ['dh_5fid',['dh_id',['../structNoiseDHState__s.html#aa92d90f9b82e88e7d7976d2e86cc2fca',1,'NoiseDHState_s::dh_id()'],['../group__names.html#a4376a3caecc734701bd5a67c865ccfb5',1,'NoiseProtocolId::dh_id()']]],
   ['dh_5flocal_5fephemeral',['dh_local_ephemeral',['../internal_8h.html#ac46bdefb4470eacb5e5d013bd3eb1d6b',1,'NoiseHandshakeState_s']]],
   ['dh_5flocal_5fstatic',['dh_local_static',['../internal_8h.html#afc890615228276fa796aaf9368c0f1d7',1,'NoiseHandshakeState_s']]],
