52e90bec06c0ba2720de9a23b512c53b028249b3
Rhys Weatherley <rhys.weatherley@gmail.com>
2016-05-21 15:45:07 +1000

    Update docs

--- search/variables_c.js
+++ search/variables_c.js
@@ -2,9 +2,13 @@ var searchData=
 [
   ['pattern_5fid',['pattern_id',['../group__names.html#a03585bc8d03a3f0cbc6dcc18f5feed02',1,'NoiseProtocolId']]],
   ['posn',['posn',['../protobufs_8h.html#a833ce6722434b7290cb291368a4856fd',1,'NoiseProtobuf']]],
+  ['pre_5fshared_5fkey',['pre_shared_key',['../internal_8h.html#adc1680b580835898b69675c11f78c559',1,'NoiseHandshakeState_s']]],
+  ['pre_5fshared_5fkey_5flen',['pre_shared_key_len',['../internal_8h.html#a2c8e78f3f83e64cbe06679a7faa2c993',1,'NoiseHandshakeState_s']]],
   ['prefix_5fid',['prefix_id',['../group__names.html#a28ed86d8adce5c81d2a47fa1e93ec74d',1,'NoiseProtocolId']]],
   ['private_5fkey',['private_key',['../structNoiseDHState__s.html#aacb825aedaa140d12fab37b897da06f0',1,'NoiseDHState_s::private_key()'],['../structNoiseSignState__s.html#a1e65cf0f1989366df94f328d65619a1d',1,'NoiseSignState_s::private_key()']]],
   ['private_5fkey_5flen',['private_key_len',['../structNoiseDHState__s.html#ad70fed9dc7645821fbf40bb89f4ab6df',1,'NoiseDHState_s::private_key_len()'],['../structNoiseSignState__s.html#abbeec938bf1948fec924cb80c36f5e81',1,'NoiseSignState_s::private_key_len()']]],
+  ['prologue',['prologue',['../internal_8h.html#a72016ecd1b44cbf226b03afefe964060',1,'NoiseHandshakeState_s']]],
+  ['prologue_5flen',['prologue_len',['../internal_8h.html#ae743824d7f470faf4aa79866f76bb962',1,'NoiseHandshakeState_s']]],
   ['public_5fkey',['public_key',['../structNoiseDHState__s.html#a63a30c2d7fc0b31b9082f4b706194b2a',1,'NoiseDHState_s::public_key()'],['../structNoiseSignState__s.html#abee6d6fc74d5a90bf771088345b9a7e5',1,'NoiseSignState_s::public_key()']]],
   ['public_5fkey_5flen',['public_key_len',['../structNoiseDHState__s.html#ab5a28a815c35396a971ee4ae533686fd',1,'NoiseDHState_s::public_key_len()'],['../structNoiseSignState__s.html#a7fe1b60adec20b128efd94601e624eb4',1,'NoiseSignState_s::public_key_len()']]]
 ];
