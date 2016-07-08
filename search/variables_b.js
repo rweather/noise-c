dddf9da47bd6e1c4ec67e3653799e2956fb7bd11
Rhys Weatherley <rhys.weatherley@gmail.com>
2016-07-09 09:49:12 +1000

    Update docs

--- search/variables_b.js
+++ search/variables_b.js
@@ -4,6 +4,5 @@ var searchData=
   ['name',['name',['../certificate_8c.html#aa26946d1486f4ecdf277c624d981a105',1,'_Noise_SubjectInfo::name()'],['../certificate_8c.html#a295cd1d5c5092c42ba922ea65240f9e8',1,'_Noise_MetaInfo::name()'],['../certificate_8c.html#a2fcca31ae0a408e5374dea8f29477549',1,'_Noise_Signature::name()'],['../certificate_8c.html#a4fc68579c02467cd68e978d7c11a2f3c',1,'_Noise_PrivateKey::name()']]],
   ['name_5fsize_5f',['name_size_',['../certificate_8c.html#adb0ff9e6c40888c30e7b86a66dac74e7',1,'_Noise_SubjectInfo::name_size_()'],['../certificate_8c.html#a1d6f45af157b670c7346e93c80cd279d',1,'_Noise_MetaInfo::name_size_()'],['../certificate_8c.html#ac6464e68f36e5359759b2f21899d0577',1,'_Noise_Signature::name_size_()'],['../certificate_8c.html#ad2c21e32a6b882a62985beedfb45318f',1,'_Noise_PrivateKey::name_size_()']]],
   ['nonce',['nonce',['../certificate_8c.html#a824d418117be5366d9b3094abaa8361b',1,'_Noise_ExtraSignedInfo']]],
-  ['nonce_5foverflow',['nonce_overflow',['../structNoiseCipherState__s.html#acfccba116293338d485ed781e2d88bbf',1,'NoiseCipherState_s']]],
   ['nonce_5fsize_5f',['nonce_size_',['../certificate_8c.html#a96940ecdcebc8961355fa731c6f38d0c',1,'_Noise_ExtraSignedInfo']]]
 ];
