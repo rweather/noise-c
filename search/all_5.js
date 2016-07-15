132baec5efc6e09187d22b706ec13c0153d17a29
Rhys Weatherley <rhys.weatherley@gmail.com>
2016-07-15 17:26:22 +1000

    Update docs

--- search/all_5.js
+++ search/all_5.js
@@ -3,6 +3,7 @@ var searchData=
   ['encrypt',['encrypt',['../structNoiseCipherState__s.html#a6d4920ec56b5a494861d62f0104f13bd',1,'NoiseCipherState_s']]],
   ['encrypted_5fdata',['encrypted_data',['../certificate_8c.html#aa2f4a8a37bde5ab88e2561e08f7cf540',1,'_Noise_EncryptedPrivateKey']]],
   ['encrypted_5fdata_5fsize_5f',['encrypted_data_size_',['../certificate_8c.html#ac0bc6ea03e282b86bc0a462c5738c3f9',1,'_Noise_EncryptedPrivateKey']]],
+  ['ephemeral_5fonly',['ephemeral_only',['../structNoiseDHState__s.html#a6d59223d17db601429afb3ee2355639f',1,'NoiseDHState_s']]],
   ['error',['error',['../protobufs_8h.html#ac2e99458e247d3a82832cea2f8227c46',1,'NoiseProtobuf']]],
   ['error_20reporting',['Error reporting',['../group__error__reports.html',1,'']]],
   ['error_20codes',['Error codes',['../group__errors.html',1,'']]],
--- search/all_c.js
+++ search/all_c.js
@@ -5,5 +5,6 @@ var searchData=
   ['max_5fsize',['max_size',['../group__buffer__management.html#aed770a48ba7a49f100b33c5853855060',1,'NoiseBuffer']]],
   ['meta',['meta',['../certificate_8c.html#a6fe7eab7251668db51b50d3391111fd9',1,'_Noise_SubjectInfo::meta()'],['../certificate_8c.html#a3d08f47dc2cdecd674958fe1b345b16a',1,'_Noise_ExtraSignedInfo::meta()'],['../certificate_8c.html#ac30eb93ee4d8359febffc46c7b4e90ef',1,'_Noise_PrivateKey::meta()']]],
   ['meta_5fcount_5f',['meta_count_',['../certificate_8c.html#aaa57d10a2039500cae1e90a653e3507b',1,'_Noise_SubjectInfo::meta_count_()'],['../certificate_8c.html#ad4a131e5f118956df9449a4b9cbf2885',1,'_Noise_ExtraSignedInfo::meta_count_()'],['../certificate_8c.html#aa71b77714dcc1efc10054cd991c6f0b1',1,'_Noise_PrivateKey::meta_count_()']]],
-  ['meta_5fmax_5f',['meta_max_',['../certificate_8c.html#a5e71c66f811cc9a3fc11b1b623eaa8e8',1,'_Noise_SubjectInfo::meta_max_()'],['../certificate_8c.html#a882d9743a12c197267acf3af7b9de7e8',1,'_Noise_ExtraSignedInfo::meta_max_()'],['../certificate_8c.html#ade2ef9db1ee73b2c5d2518f5fa31c9aa',1,'_Noise_PrivateKey::meta_max_()']]]
+  ['meta_5fmax_5f',['meta_max_',['../certificate_8c.html#a5e71c66f811cc9a3fc11b1b623eaa8e8',1,'_Noise_SubjectInfo::meta_max_()'],['../certificate_8c.html#a882d9743a12c197267acf3af7b9de7e8',1,'_Noise_ExtraSignedInfo::meta_max_()'],['../certificate_8c.html#ade2ef9db1ee73b2c5d2518f5fa31c9aa',1,'_Noise_PrivateKey::meta_max_()']]],
+  ['mutual',['mutual',['../structNoiseDHState__s.html#a0ea28f620c13bf75263ec9b68b0efd91',1,'NoiseDHState_s']]]
 ];
