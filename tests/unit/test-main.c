795fa1fdb4142c53bc22c7f8c6b021c3e5427000
Pierre Collet <pierre.collet@ledger.fr>
2020-12-09 10:52:15 +0100

    Remove protobuf from tests

--- tests/unit/test-main.c
+++ tests/unit/test-main.c
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
     test(hashstate);
     test(names);
     test(patterns);
-    test(protobufs);
+    /* test(protobufs); */
     test(randstate);
     test(signstate);
     test(symmetricstate);
